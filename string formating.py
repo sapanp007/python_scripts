6b54949586436b95769767832ea73a185637f4a6
sapanp007 <sapanp007@gmail.com>
2021-04-01 12:57:15 +0530

    -

---
+++ string formating.py
@@ -0,0 +1,22 @@
+import re
+
+
+def replace_number_with_letter(data):
+    match = data.group(0)
+    letters = re.search(r"[a-zA-Z]+", match).group()
+    number = int(re.search(r"\d", match).group())
+    return letters * number
+
+
+def format_string(string_data):
+    pattern = re.compile(r"(\d\[[a-zA-Z]+\])")
+    string_data = re.sub(pattern, replace_number_with_letter, string_data)
+    if string_data.find("[") > 0:
+        return format_string(string_data)
+    else:
+        return string_data
+
+
+if __name__ == "__main__":
+    str1 = "3[a2[c]]"
+    print(format_string(str1))
